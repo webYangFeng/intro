4d7aa4429e58162d3694f19ec0294f8edb5ec98c
YangF <17611773761@163.com>
2026-03-05 16:20:05 +0800

    feat：个人介绍

--- dist/dev/static/chunks/Desktop_my_my-app_src_app_favicon_ico_mjs_2188784d._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/f268a_next_dist_614815ba._.js"
-],
-    source: "dynamic"
-});
--- dist/dev/static/chunks/Desktop_my_my-app_src_app_layout_tsx_02242686._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__d7a0c3cd._.css",
-  "static/chunks/f268a_66465d1c._.js",
-  "static/chunks/Desktop_my_my-app_src_a8500d81._.js"
-],
-    source: "dynamic"
-});
